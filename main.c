e8fb9d75211e63004b7bfd7674ce3134679b225e
hectoriabandini <abuhmoseji@gmail.com>
2023-06-22 15:50:57 -0700

    free glo

--- main.c
+++ main.c
@@ -1,31 +1,33 @@
 #include "monty.h"
 
+
+
 /**
- * free_glo - frees the global variables
+ * free_fglo - frees the global variables
  *
  * Return: no return
  */
-void free_glo(void)
+void free_fglo(void)
 {
-	free_dlistint(glo.head);
-	free(glo.buffer);
-	fclose(glo.fdt);
+	free_dlistint(fglo.head);
+	free(fglo.buffer);
+	fclose(fglo.fd);
 }
 
 /**
- * start_glo - initializes the global variables
+ * start_fglo - initializes the global variables
  *
- * @fdt: file descriptor
+ * @fd: file descriptor
  * Return: no return
  */
-void start_glo(FILE *fdt)
+void start_fglo(FILE *fd)
 {
-	glo.lifo = 1;
-	glo.cont = 1;
-	glo.arg = NULL;
-	glo.head = NULL;
-	glo.fdt = fdt;
-	glo.buffer = NULL;
+	fglo.lifo = 1;
+	fglo.cont = 1;
+	fglo.arg = NULL;
+	fglo.head = NULL;
+	fglo.fd = fd;
+	fglo.buffer = NULL;
 }
 
 /**
@@ -38,7 +40,7 @@ void start_glo(FILE *fdt)
  */
 FILE *check_input(int argc, char *argv[])
 {
-	FILE *fdt;
+	FILE *fd;
 
 	if (argc == 1 || argc > 2)
 	{
@@ -46,15 +48,15 @@ FILE *check_input(int argc, char *argv[])
 		exit(EXIT_FAILURE);
 	}
 
-	fdt = fopen(argv[1], "r");
+	fd = fopen(argv[1], "r");
 
-	if (fdt == NULL)
+	if (fd == NULL)
 	{
 		dprintf(2, "Error: Can't open file %s\n", argv[1]);
 		exit(EXIT_FAILURE);
 	}
 
-	return (fdt);
+	return (fd);
 }
 
 /**
@@ -67,36 +69,35 @@ FILE *check_input(int argc, char *argv[])
 int main(int argc, char *argv[])
 {
 	void (*f)(stack_t **stack, unsigned int line_number);
-	FILE *fdt;
+	FILE *fd;
 	size_t size = 256;
 	ssize_t nlines = 0;
 	char *lines[2] = {NULL, NULL};
 
-	fdt = check_input(argc, argv);
-	start_glo(fdt);
-	nlines = getline(&glo.buffer, &size, fdt);
+	fd = check_input(argc, argv);
+	start_fglo(fd);
+	nlines = getline(&fglo.buffer, &size, fd);
 	while (nlines != -1)
 	{
-		lines[0] = _strtoky(glo.buffer, " \t\n");
+		lines[0] = _strtoky(fglo.buffer, " \t\n");
 		if (lines[0] && lines[0][0] != '#')
 		{
 			f = get_opcodes(lines[0]);
 			if (!f)
 			{
-				dprintf(2, "L%u: ", glo.cont);
+				dprintf(2, "L%u: ", fglo.cont);
 				dprintf(2, "unknown instruction %s\n", lines[0]);
-				free_glo();
+				free_fglo();
 				exit(EXIT_FAILURE);
 			}
-			glo.arg = _strtoky(NULL, " \t\n");
-			f(&glo.head, glo.cont);
+			fglo.arg = _strtoky(NULL, " \t\n");
+			f(&fglo.head, fglo.cont);
 		}
-		nlines = getline(&glo.buffer, &size, fdt);
-		glo.cont++;
+		nlines = getline(&fglo.buffer, &size, fd);
+		fglo.cont++;
 	}
 
-	free_glo();
+	free_fglo();
 
 	return (0);
 }
-
