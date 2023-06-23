6d4b680cf520f58dc567896c194ab553d94bd624
Michafolab <topsonmich1@gmail.com>
2023-06-22 18:39:59 -0700

    The header file holder/library

--- main.c
+++ main.c
@@ -5,31 +5,31 @@
 
 int main(int argc, char *argv[])
 {
-    char *content = NULL;
-    FILE *file;
-    char buffer[BUFFER_SIZE];
-    stack_t *stack = NULL;
-    unsigned int counter = 0;
+	char *content = NULL;
+	FILE *file;
+	char buffer[BUFFER_SIZE];
+	stack_t *stack = NULL;
+	unsigned int counter = 0;
 
-    if (argc != 2)
-    {
-        fprintf(stderr, "USAGE: monty file\n");
-        exit(EXIT_FAILURE);
-    }
-    file = fopen(argv[1], "r");
-    bus.file = file;
-    if (!file)
-    {
-        fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
-        exit(EXIT_FAILURE);
-    }
-    while (fgets(buffer, BUFFER_SIZE, file))
-    {
-        content = buffer;
-        counter++;
-        execute(content, &stack, counter, file);
-    }
-    free_stack(stack);
-    fclose(file);
-    return 0;
+	if (argc != 2)
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
+	file = fopen(argv[1], "r");
+	bus.file = file;
+	if (!file)
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
+	while (fgets(buffer, BUFFER_SIZE, file))
+	{
+		content = buffer;
+		counter++;
+		execute(content, &stack, counter, file);
+	}
+	free_stack(stack);
+	fclose(file);
+	return (0);
 }
