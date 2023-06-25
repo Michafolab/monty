fa37178e33fe637b19364b5e691b8c3c8ae31216
hectoriabandini <abuhmoseji@gmail.com>
2023-06-25 07:47:53 -0700

    pcode

--- pcode_instructions.c
+++ pcode_instructions.c
@@ -1,7 +1,7 @@
 #include "monty.h"
 
 /**
- * _push - pushes element to the stack
+ * _push - pushes an element to the stack
  *
  * @doubly: head of the linked list
  * @cline: line number
@@ -9,33 +9,33 @@
  */
 void _push(stack_t **doubly, unsigned int cline)
 {
-	int n, j;
+        int n, j;
 
-	if (!vglo.arg)
-	{
-		dprintf(2, "L%u: ", cline);
-		dprintf(2, "usage: push integer\n");
-		free_vglo();
-		exit(EXIT_FAILURE);
-	}
+        if (!vglo.arg)
+        {
+                dprintf(2, "L%u: ", cline);
+                dprintf(2, "usage: push integer\n");
+                free_vglo();
+                exit(EXIT_FAILURE);
+        }
 
-	for (j = 0; vglo.arg[j] != '\0'; j++)
-	{
-		if (!isdigit(vglo.arg[j]) && vglo.arg[j] != '-')
-		{
-			dprintf(2, "L%u: ", cline);
-			dprintf(2, "usage: push integer\n");
-			free_vglo();
-			exit(EXIT_FAILURE);
-		}
-	}
+        for (j = 0; vglo.arg[j] != '\0'; j++)
+        {
+                if (!isdigit(vglo.arg[j]) && vglo.arg[j] != '-')
+                {
+                        dprintf(2, "L%u: ", cline);
+                        dprintf(2, "usage: push integer\n");
+                        free_vglo();
+                        exit(EXIT_FAILURE);
+                }
+        }
 
-	n = atoi(vglo.arg);
+        n = atoi(vglo.arg);
 
-	if (vglo.lifo == 1)
-		add_dnodeint(doubly, n);
-	else
-		add_dnodeint_end(doubly, n);
+        if (vglo.lifo == 1)
+                add_dnodeint(doubly, n);
+        else
+                add_dnodeint_end(doubly, n);
 }
 
 /**
@@ -47,16 +47,16 @@ void _push(stack_t **doubly, unsigned int cline)
  */
 void _pall(stack_t **doubly, unsigned int cline)
 {
-	stack_t *aux;
-	(void)cline;
+        stack_t *aux;
+        (void)cline;
 
-	aux = *doubly;
+        aux = *doubly;
 
-	while (aux)
-	{
-		printf("%d\n", aux->n);
-		aux = aux->next;
-	}
+        while (aux)
+        {
+                printf("%d\n", aux->n);
+                aux = aux->next;
+        }
 }
 
 /**
@@ -68,17 +68,17 @@ void _pall(stack_t **doubly, unsigned int cline)
  */
 void _pint(stack_t **doubly, unsigned int cline)
 {
-	(void)cline;
+        (void)cline;
 
-	if (*doubly == NULL)
-	{
-		dprintf(2, "L%u: ", cline);
-		dprintf(2, "can't pint, stack empty\n");
-		free_vglo();
-		exit(EXIT_FAILURE);
-	}
+        if (*doubly == NULL)
+        {
+                dprintf(2, "L%u: ", cline);
+                dprintf(2, "can't pint, stack empty\n");
+                free_vglo();
+                exit(EXIT_FAILURE);
+        }
 
-	printf("%d\n", (*doubly)->n);
+        printf("%d\n", (*doubly)->n);
 }
 
 /**
@@ -90,17 +90,17 @@ void _pint(stack_t **doubly, unsigned int cline)
  */
 void _pop(stack_t **doubly, unsigned int cline)
 {
-	stack_t *aux;
+        stack_t *aux;
 
-	if (doubly == NULL || *doubly == NULL)
-	{
-		dprintf(2, "L%u: can't pop an empty stack\n", cline);
-		free_vglo();
-		exit(EXIT_FAILURE);
-	}
-	aux = *doubly;
-	*doubly = (*doubly)->next;
-	free(aux);
+        if (doubly == NULL || *doubly == NULL)
+        {
+                dprintf(2, "L%u: can't pop an empty stack\n", cline);
+                free_vglo();
+                exit(EXIT_FAILURE);
+        }
+        aux = *doubly;
+        *doubly = (*doubly)->next;
+        free(aux);
 }
 
 /**
@@ -112,25 +112,25 @@ void _pop(stack_t **doubly, unsigned int cline)
  */
 void _swap(stack_t **doubly, unsigned int cline)
 {
-	int m = 0;
-	stack_t *aux = NULL;
+        int m = 0;
+        stack_t *aux = NULL;
 
-	aux = *doubly;
+        aux = *doubly;
 
-	for (; aux != NULL; aux = aux->next, m++)
-		;
+        for (; aux != NULL; aux = aux->next, m++)
+                ;
 
-	if (m < 2)
-	{
-		dprintf(2, "L%u: can't swap, stack too short\n", cline);
-		free_vglo();
-		exit(EXIT_FAILURE);
-	}
+        if (m < 2)
+        {
+                dprintf(2, "L%u: can't swap, stack too short\n", cline);
+                free_vglo();
+                exit(EXIT_FAILURE);
+        }
 
-	aux = *doubly;
-	*doubly = (*doubly)->next;
-	aux->next = (*doubly)->next;
-	aux->prev = *doubly;
-	(*doubly)->next = aux;
-	(*doubly)->prev = NULL;
+        aux = *doubly;
+        *doubly = (*doubly)->next;
+        aux->next = (*doubly)->next;
+        aux->prev = *doubly;
+        (*doubly)->next = aux;
+        (*doubly)->prev = NULL;
 }
