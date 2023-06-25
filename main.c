02acec9b9caa8d403506df7a08c4f8bec414a0e3
hectoriabandini <abuhmoseji@gmail.com>
2023-06-25 07:35:47 -0700

    main.c

--- main.c
+++ main.c
@@ -9,15 +9,11 @@ global_t vglo;
  */
 void free_vglo(void)
 {
-	free_dlistint(vglo.head);
-	free(vglo.buffer);
-	fclose(vglo.fd);
+        free_dlistint(vglo.head);
+        free(vglo.buffer);
+        fclose(vglo.fd);
 }
 
-<<<<<<< HEAD
-const bus_t bus = {NULL, NULL, NULL,
-int main(int argc, char *argv[])
-=======
 /**
  * start_vglo - initializes the global variables
  *
@@ -26,12 +22,12 @@ int main(int argc, char *argv[])
  */
 void start_vglo(FILE *fd)
 {
-	vglo.lifo = 1;
-	vglo.cont = 1;
-	vglo.arg = NULL;
-	vglo.head = NULL;
-	vglo.fd = fd;
-	vglo.buffer = NULL;
+        vglo.lifo = 1;
+        vglo.cont = 1;
+        vglo.arg = NULL;
+        vglo.head = NULL;
+        vglo.fd = fd;
+        vglo.buffer = NULL;
 }
 
 /**
@@ -43,25 +39,24 @@ void start_vglo(FILE *fd)
  * Return: file struct
  */
 FILE *check_input(int argc, char *argv[])
->>>>>>> b663fce587e2bbc127cef053ac4a14611dce0fa6
 {
-	FILE *fd;
+        FILE *fd;
 
-	if (argc == 1 || argc > 2)
-	{
-		dprintf(2, "USAGE: monty file\n");
-		exit(EXIT_FAILURE);
-	}
+        if (argc == 1 || argc > 2)
+        {
+                dprintf(2, "USAGE: monty file\n");
+                exit(EXIT_FAILURE);
+        }
 
-	fd = fopen(argv[1], "r");
+        fd = fopen(argv[1], "r");
 
-	if (fd == NULL)
-	{
-		dprintf(2, "Error: Can't open file %s\n", argv[1]);
-		exit(EXIT_FAILURE);
-	}
+        if (fd == NULL)
+        {
+                dprintf(2, "Error: Can't open file %s\n", argv[1]);
+                exit(EXIT_FAILURE);
+        }
 
-	return (fd);
+        return (fd);
 }
 
 /**
@@ -73,41 +68,36 @@ FILE *check_input(int argc, char *argv[])
  */
 int main(int argc, char *argv[])
 {
-	void (*f)(stack_t **stack, unsigned int line_number);
-	FILE *fd;
-	size_t size = 256;
-	ssize_t nlines = 0;
-	char *lines[2] = {NULL, NULL};
+        void (*f)(stack_t **stack, unsigned int line_number);
+        FILE *fd;
+        size_t size = 256;
+        ssize_t nlines = 0;
+        char *lines[2] = {NULL, NULL};
 
-	fd = check_input(argc, argv);
-	start_vglo(fd);
-	nlines = getline(&vglo.buffer, &size, fd);
-	while (nlines != -1)
-	{
-		lines[0] = _strtoky(vglo.buffer, " \t\n");
-		if (lines[0] && lines[0][0] != '#')
-		{
-			f = get_opcodes(lines[0]);
-			if (!f)
-			{
-				dprintf(2, "L%u: ", vglo.cont);
-				dprintf(2, "unknown instruction %s\n", lines[0]);
-				free_vglo();
-				exit(EXIT_FAILURE);
-			}
-			vglo.arg = _strtoky(NULL, " \t\n");
-			f(&vglo.head, vglo.cont);
-		}
-		nlines = getline(&vglo.buffer, &size, fd);
-		vglo.cont++;
-	}
-<<<<<<< HEAD
-	free_stack(stack);
-	fclose(file);
-=======
+        fd = check_input(argc, argv);
+        start_vglo(fd);
+        nlines = getline(&vglo.buffer, &size, fd);
+        while (nlines != -1)
+        {
+                lines[0] = _strtoky(vglo.buffer, " \t\n");
+                if (lines[0] && lines[0][0] != '#')
+                {
+                        f = get_opcodes(lines[0]);
+                        if (!f)
+                        {
+                                dprintf(2, "L%u: ", vglo.cont);
+                                dprintf(2, "unknown instruction %s\n", lines[0]);
+                                free_vglo();
+                                exit(EXIT_FAILURE);
+                        }
+                        vglo.arg = _strtoky(NULL, " \t\n");
+                        f(&vglo.head, vglo.cont);
+                }
+                nlines = getline(&vglo.buffer, &size, fd);
+                vglo.cont++;
+        }
 
-	free_vglo();
->>>>>>> b663fce587e2bbc127cef053ac4a14611dce0fa6
+        free_vglo();
 
-	return (0);
+        return (0);
 }
